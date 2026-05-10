c379acf345d6989e2d9e37fbdeb58d71c5d46294
gui <gui.polezi@hotmail.com>
2026-05-10 16:42:28 -0300

    random changes trying to fix bug supabase... JUST RLS

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -5,6 +5,7 @@ const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+
 /*
 async function testarConexao() {
   const { data, error } = await supabase.from('usuarios').select('*');
