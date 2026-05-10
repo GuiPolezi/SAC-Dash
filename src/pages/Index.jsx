68dac804e9f822dd9a816d503de514d438531dbc
gui <gui.polezi@hotmail.com>
2026-05-10 16:50:22 -0300

    Added Create System

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,6 +1,7 @@
 import {Logout} from '../components/Logout'
 import { Link } from 'react-router-dom'
 import { CriarSuporte } from '../components/Suporte'
+import { CriarSistema } from '../components/Sistema'
 
 export function Home() {
     return (
@@ -9,6 +10,7 @@ export function Home() {
         <Logout />
 
         <CriarSuporte />
+        <CriarSistema />
         </>
     )
 }
--- src/services/dbService.js
+++ src/services/dbService.js
@@ -5,19 +5,30 @@ import { supabase } from './supabase'
 export const dbService = {
 
     // Criar suporte
-   async criarSuporte(nome, sexo, data_nascimento) {
-    const { data: { user }, error: authError } = await supabase.auth.getUser()
+    async criarSuporte(nome, sexo, data_nascimento) {
+        const { data: { user }, error: authError } = await supabase.auth.getUser()
 
-    if (!user) {
-        throw new Error('Usuário não autenticado. Faça login antes de inserir.')
+        if (!user) {
+            throw new Error('Usuário não autenticado. Faça login antes de inserir.')
+        }
+
+        const { data, error } = await supabase
+            .from('suporte')
+            .insert([{ nome, sexo, data_nascimento }])
+            .select()
+
+        if (error) throw error
+        return data[0]
+    },
+
+    // Criar sistema
+    async criarSistema(tipo, nome_sistema) {
+        const { data, error } = await supabase
+            .from('sistemas')
+            .insert([{ tipo, nome_sistema }])
+            .select()
+        if (error) throw error
+        return data[0]
     }
 
-    const { data, error } = await supabase
-        .from('suporte')
-        .insert([{ nome, sexo, data_nascimento }])
-        .select()
-
-    if (error) throw error
-    return data[0]
-}
 }
